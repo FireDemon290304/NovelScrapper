b6594bb9fe231670457e5fe8b9afa3fe37df82a3
FireDemon290304 <139615375+FireDemon290304@users.noreply.github.com>
2023-07-22 19:22:23 +0200

    Update utils.py
    added verbose support

--- utils.py
+++ utils.py
@@ -5,48 +5,53 @@ def timeme(method):
     """
     A decorator that measures the execution time of a function and prints it out.
 
-    Parameters:
-    - method: the function to be decorated
+    Parameters:\n
+    - method: the function to be decorated\n
 
-    Optional parameters:
-    - threshold: the time threshold in seconds (default: 10)
-    - format: the output format ('hmsr' for hours, minutes, seconds, and milliseconds,
-              's' for seconds rounded to five decimal places; default: 'hmsr')
+    Optional parameters:\n
+    - threshold: the time threshold in seconds (default: 10)\n
+    - time_format: the output format ('hmsr' for hours, minutes, seconds, and milliseconds, 's' for seconds rounded to five decimal places; default: 'hmsr')\n
+    - verbose: print execution time. Default is true
     """
 
     def wrapper(*args, **kw):
+        verbose = kw.get('verbose', True)  # Get the 'verbose' flag, default is True
         threshold = kw.pop('threshold', 10)
-        output_format = kw.pop('format', 'hmsr')
-        if threshold is None: threshold = 10
-        if output_format is None: output_format = 'hmsr'
+        time_format = kw.pop('output_format', 'hmsr')
+        if threshold is None:
+            threshold = 10
+        if time_format is None:
+            time_format = 'hmsr'
 
         start_time = time.time()
         try:
             result = method(*args, **kw)
         except Exception as e:
             end_time = time.time()
-            print(f'{method.__name__} raised an exception: {e}.')  # Not necessary
+            if verbose:
+                print(f'{method.__name__} raised an exception: {e}.')
             raise
         else:
             end_time = time.time()
 
-        elapsed_time = round(end_time - start_time, 4)
-        if elapsed_time > threshold:
-            if output_format == 'hmsr':
-                hours, remainder = divmod(elapsed_time, 3600)
-                minutes, remainder = divmod(remainder, 60)
-                seconds, milliseconds = divmod(remainder, 1)
-                milliseconds = int(milliseconds * 1000)
-                time_str = f'{hours:.0f}:{minutes:02.0f}:{seconds:02.0f}:{milliseconds:03d}'
-            elif output_format == 's':
-                time_str = f'{elapsed_time:.5f}'
+        if verbose:  # If 'verbose' flag is True, print the execution time
+            elapsed_time = round(end_time - start_time, 4)
+            if elapsed_time > threshold:
+                if time_format == 'hmsr':
+                    hours, remainder = divmod(elapsed_time, 3600)
+                    minutes, remainder = divmod(remainder, 60)
+                    seconds, milliseconds = divmod(remainder, 1)
+                    milliseconds = int(milliseconds * 1000)
+                    time_str = f'{hours:.0f}:{minutes:02.0f}:{seconds:02.0f}:{milliseconds:03d}'
+                elif time_format == 's':
+                    time_str = f'{elapsed_time:.5f}'
+                else:
+                    raise ValueError(f'Invalid output format: {time_format}')
             else:
-                raise ValueError(f'Invalid output format: {output_format}')
-        else:
-            time_str = f'{elapsed_time:.5f}'
+                time_str = f'{elapsed_time:.5f}'
 
-        class_name = type(args[0]).__name__ if args else ''
-        print(f'{class_name + "." if class_name else ""}{method.__name__} ran for {time_str} seconds.')
+            class_name = type(args[0]).__name__ if args else ''
+            print(f'{class_name + "." if class_name else ""}{method.__name__} ran for {time_str} seconds.')
 
         return result
     return wrapper
